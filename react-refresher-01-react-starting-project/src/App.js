19420e2692b0291ff83b5e7c3010e602e6db431d
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-09-06 13:19:46 +0300

    add

---
+++ react-refresher-01-react-starting-project/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import GoalList from './components/GoalList'
+
+const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn all about the course main topic'},
+    {id: 'cg3', text: 'Help other students in the course Q&A'}
+  ];
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals}/>
+    </div>
+  );
+};
+
+export default App;
